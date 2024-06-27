0e835612809d3a80738279236e89d39c6115f4d3
kaboko14 <aokitori6211@gmail.com>
2024-06-27 15:30:57 +0900

    Link名前付きルート

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+<h1>aaa</h1>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+Inertiaテストです。<br>
+<a href="/">aタグ経由</a><br>
+<Link href="/">Link経由</Link><br>
+<!-- 名前付きルートを指定できる -->
+<Link :href="route('inertia.index')">名前付きルートの確認</Link><br>
+</template>
