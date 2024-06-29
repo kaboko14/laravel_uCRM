7693a50c9166dfaf8d0360908c4a57503f509f5b
kaboko14 <aokitori6211@gmail.com>
2024-06-29 17:01:57 +0900

    emit

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,12 +2,17 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue'
 import Label from '@/Components/InputLabel.vue'
 import Input from '@/Components/TextInput.vue'
+const emitTest = e => console.log(e);
 </script>
 
 <template>
 <GuestLayout>
     <Label value="件名">タイトル</Label>
-    <Input modelValue="初期値がはいる"></Input>
+    <Input 
+        modelValue="初期値がはいる"
+        @update:modelValue="emitTest"
+    >
+</Input>
 </GuestLayout>
 
 </template>
