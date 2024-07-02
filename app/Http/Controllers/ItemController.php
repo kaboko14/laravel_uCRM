de670bc2a46d164462269e2aeea6c2ac083f8659
kaboko14 <aokitori6211@gmail.com>
2024-07-02 10:56:16 +0900

    商品追加ページ

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -15,7 +16,12 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::select('id', 'name', 'price', 'is_selling')
+        ->get();
+        
+        return Inertia::render('Items/Index', [
+            'items' => $items
+        ]);
     }
 
     /**
@@ -25,7 +31,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -36,7 +42,17 @@ class ItemController extends Controller
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+        return to_route('items.index')
+        ->with([
+            'message' => '登録しました。',
+            'status' => 'success'
+        ]);
     }
 
     /**
