ccab46f80a503119095b0952db9f8684392f2dbf
kaboko14 <aokitori6211@gmail.com>
2024-07-02 17:16:28 +0900

    商品関連

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -9,4 +9,10 @@
 >
     {{ $page.props.flash.message }}
 </div>
+<div 
+    v-if="$page.props.flash.status === 'danger'" 
+    class="bg-red-300 text-white p-4"
+>
+    {{ $page.props.flash.message }}
+</div>
 </template>
