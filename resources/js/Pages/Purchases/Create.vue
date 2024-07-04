a0e4fc66549418192c856e5b58d39c8470e20cda
kaboko14 <aokitori6211@gmail.com>
2024-07-04 15:04:05 +0900

    モーダルの追加

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -1,11 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import InputError from '@/Components/InputError.vue'
 import { Head } from '@inertiajs/vue3';
 import { onMounted, reactive, ref, computed } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import { getToday } from '@/common';
-
+import MicroModal from '@/Components/MicroModal.vue';
 
 const props = defineProps({
     customers: Array,
@@ -83,6 +82,7 @@ const form = reactive({
                                             <!-- 会員名 -->
                                             <div class="p-2 w-full">
                                                 <div class="relative">
+                                                    <MicroModal />
                                                     <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
                                                     <select id="customer" name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                         <option v-for="customer in customers" :value="customer.id" :key="customer.id">
